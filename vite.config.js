4a842f53a8fa6c27503f4791025913f8acec6916
Treorge <1515592489@qq.com>
2024-11-07 15:25:45 +0800

    主题色定制

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        //配置element plus采用scss样式配色
+        ElementPlusResolver({
+          importStyle: 'sass',
+          // directives: true,
+          // version: "2.1.5",
+        }),
+      ],
+    }),
+    ElementPlus({
+      useSource: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
     },
   },
 })
