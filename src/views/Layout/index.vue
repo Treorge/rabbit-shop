71b48542b3a6dfd554c2a493ed157fc40ca8bdf9
Treorge <1515592489@qq.com>
2024-11-11 11:24:28 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,16 @@
+<script setup>
+import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
+import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+</script>
+
 <template>
   <div>
-    首页 
+    首页
     <!-- 二级路由出口 -->
+    <LayoutNav />
+    <LayoutHeader />
     <RouterView />
+    <LayoutFooter />
   </div>
 </template>
