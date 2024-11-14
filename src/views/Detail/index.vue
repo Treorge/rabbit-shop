876b8d0d78de39679198ee901f6198c4a3457b67
Treorge <1515592489@qq.com>
2024-11-14 17:38:05 +0800

    详情页图片预览

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,7 +3,7 @@ import { getDetail } from '@/api/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
-
+import ImageView from '@/components/ImageView/index.vue'
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -35,7 +35,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
