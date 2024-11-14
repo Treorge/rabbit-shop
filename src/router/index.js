45310c377fb07a5d00ea66589434fb9e8fd7d5ce
Treorge <1515592489@qq.com>
2024-11-14 15:48:02 +0800

    详情页热榜区渲染

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const Login = () => import('@/views/Login/index.vue')
 const Home = () => import('@/views/Home/index.vue')
 const Category = () => import('@/views/Category/index.vue')
 const SubCategory = () => import('@/views/SubCategory/index.vue')
+const Detail = () => import('@/views/Detail/index.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
           name: 'subCategory',
           component: SubCategory,
         },
+        {
+          path: 'detail/:id',
+          name: 'detail',
+          component: Detail,
+        },
       ],
     },
     {
@@ -35,6 +41,12 @@ const router = createRouter({
       component: Login,
     },
   ],
+  //路由滚动行为
+  scrollBehavior() {
+    return {
+      top: 0,
+    }
+  },
 })
 
 export default router
